6671b773274993c44c1a942ca9e806bfe1e59cc6
IgorShebuldaev <igor.shebuldaev@gmail.com>
2022-04-07 21:38:33 +0300

    Result dice in history activity change to img.

--- app/src/main/java/org/dev/diceroller/adapter/MyRecyclerViewAdapter.kt
+++ app/src/main/java/org/dev/diceroller/adapter/MyRecyclerViewAdapter.kt
@@ -3,8 +3,8 @@ package org.dev.diceroller.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import org.dev.diceroller.R
 import org.dev.diceroller.model.DiceResult
@@ -15,7 +15,7 @@ class MyRecyclerViewAdapter(private val diceResultList: List<DiceResult>) : Recy
 
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val tvRollNumber: TextView = itemView.findViewById(R.id.tvRollNumber)
-        val tvRollResult: TextView = itemView.findViewById(R.id.tvRollResult)
+        val ivRollResult: ImageView = itemView.findViewById(R.id.ivRollResult)
         val tvRollTime: TextView = itemView.findViewById(R.id.tvRollTime)
     }
 
@@ -34,7 +34,18 @@ class MyRecyclerViewAdapter(private val diceResultList: List<DiceResult>) : Recy
         val diceResult = diceResultList[position]
 
         holder.tvRollNumber.text = diceResult.rollNumber.toString()
-        holder.tvRollResult.text = diceResult.rollResult.toString()
+        holder.ivRollResult.setImageResource(getResourceValue(diceResult.rollResult))
         holder.tvRollTime.text = SimpleDateFormat("HH:mm:ss", Locale.ENGLISH).format(diceResult.rollTime).toString()
+    }
+
+    private fun getResourceValue(number: Int): Int {
+        return when (number) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 }
